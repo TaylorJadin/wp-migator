41cfabb4d8778f755f5cb98736cda449eebc091d
Taylor Jadin <jadin.taylor@gmail.com>
2024-08-08 16:02:14 -0500

    delet db_file

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+bash <(curl -s https://raw.githubusercontent.com/TaylorJadin/wp-migator/main/wpmigator.sh)
+```
--- wpmigator.sh
+++ wpmigator.sh
@@ -4,9 +4,9 @@
 webroot=/var/www/webroot
 
 # Input
-read -p "Source server: " source_server
-read -p "Source path: " raw_path
-read -p "Source username: " source_username
+read -p "Source server: " SOURCE_SERVER
+read -p "Source path: " RAW_PATH
+read -p "Source username: " SOURCE_USERNAME
 
 # Validation
 ensure_trailing_slash() {
@@ -17,16 +17,17 @@ ensure_trailing_slash() {
     echo "${path}"
 }
 
-source_path=$(ensure_trailing_slash "$raw_path")
-db_file=$source_username-$(date +'%Y-%m-%dT%H%M%S%z').sql
+SOURCE_PATH=$(ensure_trailing_slash "$RAW_PATH")
+DB_FILE=$SOURCE_USERNAME-$(date +'%Y-%m-%dT%H%M%S%z').sql
 
 cp $webroot/.htaccess /tmp/.htaccess
 cp /var/www/webroot/ROOT/wp-config.php /tmp/wp-config.php
-ssh $source_username@$source_server "cd $source_path && wp db export $db_file"
-rsync -ahP --delete $source_username@$source_server:$source_path $webroot/
+ssh $SOURCE_USERNAME@$SOURCE_SERVER "cd $SOURCE_PATH && wp db export $DB_FILE"
+rsync -ahP --delete $SOURCE_USERNAME@$SOURCE_SERVER:$SOURCE_PATH $webroot/
 chmod 600 $webroot/wp-config.php
 cp $webroot/.htaccess $webroot/.htaccess.bak
 cp $webroot/wp-config.php $webroot/wp-config.php.bak
 cp /tmp/.htaccess $webroot/.htaccess
 cp /tmp/wp-config.php $webroot/wp-config.php
-cd $webroot && wp db import $db_file && wp option update upload_path ''
+cd $webroot && wp db import $DB_FILE && wp option update upload_path ''
+ssh $SOURCE_USERNAME@$SOURCE_SERVER "cd $SOURCE_PATH && rm $DB_FILE"
