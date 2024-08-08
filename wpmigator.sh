a611f04113836c06a349a77d277eba07dff4df82
Taylor Jadin <jadin.taylor@gmail.com>
2024-08-08 16:14:17 -0500

    var names

--- wpmigator.sh
+++ wpmigator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Contstants
-webroot=/var/www/webroot
+WEBROOT=/var/www/WEBROOT
 
 # Input
 read -p "Source server: " SOURCE_SERVER
@@ -20,14 +20,14 @@ ensure_trailing_slash() {
 SOURCE_PATH=$(ensure_trailing_slash "$RAW_PATH")
 DB_FILE=$SOURCE_USERNAME-$(date +'%Y-%m-%dT%H%M%S%z').sql
 
-cp $webroot/.htaccess /tmp/.htaccess
-cp /var/www/webroot/ROOT/wp-config.php /tmp/wp-config.php
+cp $WEBROOT/.htaccess /tmp/.htaccess
+cp $WEBROOT/ROOT/wp-config.php /tmp/wp-config.php
 ssh $SOURCE_USERNAME@$SOURCE_SERVER "cd $SOURCE_PATH && wp db export $DB_FILE"
-rsync -ahP --delete $SOURCE_USERNAME@$SOURCE_SERVER:$SOURCE_PATH $webroot/
-chmod 600 $webroot/wp-config.php
-cp $webroot/.htaccess $webroot/.htaccess.bak
-cp $webroot/wp-config.php $webroot/wp-config.php.bak
-cp /tmp/.htaccess $webroot/.htaccess
-cp /tmp/wp-config.php $webroot/wp-config.php
-cd $webroot && wp db import $DB_FILE && wp option update upload_path ''
+rsync -ahP --delete $SOURCE_USERNAME@$SOURCE_SERVER:$SOURCE_PATH $WEBROOT/
+chmod 600 $WEBROOT/wp-config.php
+cp $WEBROOT/.htaccess $WEBROOT/.htaccess.bak
+cp $WEBROOT/wp-config.php $WEBROOT/wp-config.php.bak
+cp /tmp/.htaccess $WEBROOT/.htaccess
+cp /tmp/wp-config.php $WEBROOT/wp-config.php
+cd $WEBROOT && wp db import $DB_FILE && wp option update upload_path ''
 ssh $SOURCE_USERNAME@$SOURCE_SERVER "cd $SOURCE_PATH && rm $DB_FILE"
